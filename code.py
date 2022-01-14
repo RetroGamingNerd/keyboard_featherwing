a9ef65a888bc07f5712b168435a2ab0ebb7dbd72
RetroGamingNerd <97601163+RetroGamingNerd@users.noreply.github.com>
2022-01-14 07:04:31 +0000

    Add files via upload

--- code.py
+++ code.py
@@ -14,6 +14,8 @@ tft_dc = board.D10
 display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs)
 display = adafruit_ili9341.ILI9341(display_bus, width=320, height=240)
 
+i2c = board.I2C()
+
 touch = tsc2004.TSC2004(i2c)
 while not touch.touched:
     pass
